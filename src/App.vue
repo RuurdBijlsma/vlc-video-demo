b4b65e6bb729914d690f736030db19b99e38c0e9
Ruurd Bijlsma <ruurd@bijlsma.dev>
2021-01-28 22:55:47 +0100

    Update to new version, copy source files for easy development

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
     <div id="app">
         <h1>VLC Video</h1>
-        <vlc-video height="300" controls ref="player" class="player" dark
+        <vlc-video ref="player" class="player" dark
+                   height="auto"
+                   controls
                    src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"/>
         <h1>HTML Video</h1>
-        <video height="300" controls class="player"
+        <video class="player"
+               controls
                src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"/>
     </div>
 </template>
 
 <script>
 import VlcVideo from "vlc-video";
+// import VlcVideo from "./components/VlcVideo";
 
 export default {
     name: 'App',
@@ -29,5 +33,9 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+}
+
+.player {
+    width: 100%;
 }
 </style>
